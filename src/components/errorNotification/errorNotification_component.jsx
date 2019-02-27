3d3a82904e66d47de30b705def6a308ec40a27f9
Mass22 <mass22@github.com>
2019-02-26 23:25:32 -0500

    update error management

--- src/components/errorNotification/errorNotification_component.jsx
+++ src/components/errorNotification/errorNotification_component.jsx
@@ -1,1 +1,30 @@
-rcc
+import React, { Component } from "react";
+import { Message } from "semantic-ui-react";
+
+class ErrorNotification extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { visible: true };
+  }
+
+  handleDismiss = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    if (this.state.visible) {
+      return (
+        <Message
+          onDismiss={this.handleDismiss}
+          warning
+          header="Warning!"
+          content={this.props.data.message}
+        />
+      );
+    } else {
+      return false;
+    }
+  }
+}
+export default ErrorNotification;
--- src/containers/temperature_list.jsx
+++ src/containers/temperature_list.jsx
@@ -1,65 +1,64 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Table, Message } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
 import GraphTemperature from "../components/Graph/graph_component";
+import ErrorNotification from "../components/errorNotification/errorNotification_component";
 const Timestamp = require("react-timestamp");
 
 class TemperatureList extends Component {
   renderTemperature(cityData) {
-    if (cityData.cod >= "400") {
+    if (cityData !== undefined || cityData !== null) {
+      const name = cityData.city.name;
+      const temps = cityData.list.map((temperature, i) => {
+        return (
+          <Table.Cell key={i}>{Math.round(temperature.temp.day)} °C</Table.Cell>
+        );
+      });
+      const date = cityData.list.map(temperature => {
+        return (
+          <Table.HeaderCell key={temperature.dt}>
+            <Timestamp time={temperature.dt} format="date" />
+          </Table.HeaderCell>
+        );
+      });
       return (
-        <Message
-          warning
-          header="Warning!"
-          content={cityData.message}
-          key={Math.random()}
-          //onDismiss={this.handleDismiss}
-        />
+        <div key={Math.random()} className="tc__item">
+          <Table stackable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>City</Table.HeaderCell>
+                {date}
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <strong>{name}</strong>
+                </Table.Cell>
+                {temps}
+              </Table.Row>
+            </Table.Body>
+          </Table>
+          <GraphTemperature cityTemps={cityData} />
+        </div>
       );
     }
-    const name = cityData.city.name;
-    const temps = cityData.list.map((temperature, i) => {
-      return (
-        <Table.Cell key={i}>{Math.round(temperature.temp.day)} °C</Table.Cell>
-      );
-    });
-    const date = cityData.list.map(temperature => {
-      return (
-        <Table.HeaderCell key={temperature.dt}>
-          <Timestamp time={temperature.dt} format="date" />
-        </Table.HeaderCell>
-      );
-    });
-
-    return (
-      <div key={Math.random()} className="tc__item">
-        <Table stackable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>City</Table.HeaderCell>
-              {date}
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>
-                <strong>{name}</strong>
-              </Table.Cell>
-              {temps}
-            </Table.Row>
-          </Table.Body>
-        </Table>
-        <GraphTemperature cityTemps={cityData} />
-      </div>
-    );
   }
 
   render() {
-    return (
-      <div className="C--table-list-container">
-        {this.props.temperature.map(this.renderTemperature)}
-      </div>
-    );
+    console.log(this.props.temperature);
+    if (this.props.temperature.cod >= "400") {
+      return (
+        <ErrorNotification data={this.props.temperature} key={Math.random()} />
+      );
+    } else {
+      return false;
+      // return (
+      //   <div className="C--table-list-container">
+      //     {this.props.temperature.map(this.renderTemperature)}
+      //   </div>
+      // );
+    }
   }
 }
 
